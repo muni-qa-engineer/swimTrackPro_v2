b07ec1894dd1b1c66d036852eec58ab4f3236afa
Munisekhar <munisekhar@Munisekhars-MacBook-Air.local>
2026-05-10 12:39:10 +0530

    Remove obsolete dashboard CSS and JS files

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-import os
+# import os
 
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
-SECRET_KEY = os.getenv("SECRET_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
+# ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+# ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "91335"
+SECRET_KEY = "swimtrack_secret_key"
+# DATABASE_URL = "postgresql://swimadmin:kuk3LlKHG0PJGcnemUVaPuTHilFiXdGl@dpg-d7v08mbeo5us73df1o2g-a/swimtrack"
+DATABASE_URL = "postgresql://swimadmin:kuk3LlKHG0PJGcnemUVaPuTHilFiXdGl@dpg-d7v08mbeo5us73df1o2g-a.singapore-postgres.render.com/swimtrack"
